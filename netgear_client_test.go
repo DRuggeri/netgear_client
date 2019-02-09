9f4e6e4a2dd2029583cea59fbf2d37fac8636588
Daniel Ruggeri <DRuggeri@primary.net>
2019-02-08 22:24:40 -0600

    Separate test cases, too

--- netgear_client_test.go
+++ netgear_client_test.go
@@ -3,7 +3,6 @@ package netgear_client
 import (
 	"os"
 	"testing"
-	"fmt"
 )
 
 func TestLogin(t *testing.T) {
@@ -21,32 +20,6 @@ func TestLogin(t *testing.T) {
 	err = client.LogIn()
 	if err != nil {
 		t.Fatalf("Error logging in: %s", err)
-	}
-}
-
-func TestGetTrafficMeterStatistics(t *testing.T) {
-	debug := true
-
-	if get_password() == "" {
-		t.Fatal("Error: NETGEAR_PASSWORD environment variable is not set")
-	}
-
-	client, err := NewNetgearClient(get_url(), true, get_username(), get_password(), 2, debug)
-	if err != nil {
-		t.Fatalf("Error getting a client: %s", err)
-	}
-
-	res, err := client.GetTrafficMeterStatistics()
-	if err != nil {
-		t.Fatalf("Error getting traffic statistics: %s", err)
-	}
-	if len(res) < 1 {
-		t.Fatalf("Result is empty... WTF!")
-	}
-	if debug {
-		for k, v := range res {
-			fmt.Printf("%v => %v\n", k, v)
-		}
 	}
 }
 
