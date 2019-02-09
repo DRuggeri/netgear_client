aa10f49ddae5c8db1bccc9c09e97c4c868b9113a
Daniel Ruggeri <DRuggeri@primary.net>
2019-02-08 22:22:06 -0600

    Begin breaking actions out into separate files

--- netgear_client.go
+++ netgear_client.go
@@ -62,22 +62,6 @@ const AUTH_REQUEST = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
   </SOAP-ENV:Body>
 </SOAP-ENV:Envelope>`
 
-const TRAFFIC_ACTION = "urn:NETGEAR-ROUTER:service:DeviceConfig:1#GetTrafficMeterStatistics"
-const TRAFFIC_REQUEST = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<SOAP-ENV:Envelope
-  xmlns:SOAPSDK1="http://www.w3.org/2001/XMLSchema"
-  xmlns:SOAPSDK2="http://www.w3.org/2001/XMLSchema-instance"
-  xmlns:SOAPSDK3="http://schemas.xmlsoap.org/soap/encoding/"
-  xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
-  <SOAP-ENV:Header>
-    <SessionID>%s</SessionID>
-  </SOAP-ENV:Header>
-  <SOAP-ENV:Body>
-    <M1:GetTrafficMeterStatistics xmlns:M1="urn:NETGEAR-ROUTER:service:DeviceConfig:1">
-    </M1:GetTrafficMeterStatistics>
-  </SOAP-ENV:Body>
-</SOAP-ENV:Envelope>`
-
 func NewNetgearClient(i_url string, i_insecure bool, i_username string, i_password string, i_timeout int, i_debug bool) (*NetgearClient, error) {
 	if i_debug {
 		log.Printf("netgear_client.go: Constructing debug client\n")
@@ -118,37 +102,6 @@ func NewNetgearClient(i_url string, i_insecure bool, i_username string, i_passwo
 func (client *NetgearClient) LogIn() error {
 	_, err := client.send_request(AUTH_ACTION, fmt.Sprintf(AUTH_REQUEST, client.sessionid, client.username, client.password), false)
 	return err
-}
-
-func (client *NetgearClient) GetTrafficMeterStatistics() (map[string]string, error) {
-	response, err := client.send_request(TRAFFIC_ACTION, fmt.Sprintf(TRAFFIC_REQUEST, client.sessionid), true)
-	if err != nil {
-		return make(map[string]string), err
-	}
-
-	var inside Node
-	err = xml.Unmarshal(response, &inside)
-	if err != nil {
-		return make(map[string]string), fmt.Errorf("Failed to unmarshal response from inside SOAP body: %v", err)
-	}
-
-	var stats = make(map[string]string)
-	for _, node := range inside.Nodes {
-		name := node.XMLName.Local
-		value := strings.Replace(node.Content, ",", "", -1)
-		if strings.HasPrefix(name, "New") {
-			name = name[3:]
-		}
-
-		idx := strings.Index(value, "/")
-		if idx > 0 {
-			stats[name+"Average"] = value[idx+1:]
-			stats[name] = value[0:idx]
-		} else {
-			stats[name] = value
-		}
-	}
-	return stats, nil
 }
 
 func (client *NetgearClient) send_request(action string, data string, attempt_login bool) ([]byte, error) {
